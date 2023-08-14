d9cda39259ff921f1e2e735707a686c8cd71bdea
Ros <rosivanyshyn@gmail.com>
2023-08-14 17:33:48 +0300

    Update Apartment Controller
    Add Apartment Details page support

--- src/main/java/ua/ros/spring/hotel/controller/other/apartment/ApartmentController.java
+++ src/main/java/ua/ros/spring/hotel/controller/other/apartment/ApartmentController.java
@@ -1,7 +1,7 @@
 package ua.ros.spring.hotel.controller.other.apartment;
 
 import com.querydsl.core.types.Predicate;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.constraints.Size;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +13,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ua.ros.spring.hotel.controller.dto.apartment.ApartmentDTO;
 import ua.ros.spring.hotel.model.entity.Apartment;
 import ua.ros.spring.hotel.model.service.ApartmentService;
 import ua.ros.spring.hotel.model.service.BookingService;
@@ -22,10 +23,8 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Optional;
 
-import static ua.ros.spring.hotel.controller.constant.ControllerConstant.APARTMENTS_HTML;
-import static ua.ros.spring.hotel.controller.constant.ControllerConstant.APARTMENT_DETAILS_HTML;
+import static ua.ros.spring.hotel.controller.constant.ControllerConstant.*;
 
 @Slf4j
 @Controller
@@ -68,6 +67,35 @@ public class ApartmentController {
        return APARTMENTS_HTML;
     }
     //TODO Добавити зберігання Sort в сесії
+
+    @GetMapping("/getApartmentDetails")
+    public String singleApartmentPage(Model model, HttpSession session,
+                                      @QuerydslPredicate(root = Apartment.class) Predicate predicate,
+                                      @RequestParam(name = "apartmentId") @NonNull Long apartmentId
+    ) {
+        log.info("GET Single Apartment. By Id " + apartmentId);
+        Apartment apartment = apartmentService.findOne(predicate).orElseThrow();
+
+        model.addAttribute("apartment", apartment);
+        model.addAttribute("currentDate", LocalDate.now());
+
+
+        HashMap<Date, Date> disabledDatesMap = setDisabledDatesOfRelatedApartmentInSession(session, apartmentId);
+        ArrayList<LocalDate> disabledDatesList = dateUtil.convertDatesRangeToList(disabledDatesMap);
+        log.info("Disabled for booking Data`s list -> " + disabledDatesList);
+        model.addAttribute("disabledDatesList", disabledDatesList);
+        return APARTMENT_DETAILS_HTML;
+    }
+
+    private HashMap<Date, Date> setDisabledDatesOfRelatedApartmentInSession(HttpSession session, Long requestedApartmentId) {
+        HashMap<Date, Date> disabledDatesMap;
+
+        disabledDatesMap = bookingService.findAllBookingsDatesRelatedToApartment(requestedApartmentId);
+        session.setAttribute("apartmentIdOfBookingDates", requestedApartmentId);
+        session.setAttribute("disabledDatesMap", disabledDatesMap);
+
+        return disabledDatesMap;
+    }
 
     private boolean isBlank(String value) {
         return value == null || value.isEmpty();
